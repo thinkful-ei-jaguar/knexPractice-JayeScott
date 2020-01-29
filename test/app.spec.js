d44a87c8542a3fab0a90455c683eceb414e9d494
Scott Lingner <slingner@gmail.com>
2020-01-29 08:09:07 -0800

    first commit

--- test/app.spec.js
+++
@@ -1,9 +0,0 @@
-const app = require('../src/app');
-
-describe('App', () => {
-  it("GET / responds 200 and 'Hello World'", () => {
-    return supertest(app)
-      .get('/')
-      .expect(200, { message: 'Hello World' });
-  });
-});
